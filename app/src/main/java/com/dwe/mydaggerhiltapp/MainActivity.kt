1fdaed69e512efef9deca16efbc958402805e2b0
Dwee <wahyudwihartarto3@gmail.com>
2022-09-22 10:21:50 +0700

    implement modules, binds, and provides

--- app/src/main/java/com/dwe/mydaggerhiltapp/MainActivity.kt
+++ app/src/main/java/com/dwe/mydaggerhiltapp/MainActivity.kt
@@ -4,76 +4,83 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    // field injection
     @Inject
     lateinit var someClass: SomeClass
-
-    @Inject
-    lateinit var singletonClass: SingletonClass
-
-    @Inject
-    lateinit var someDependencyClass: SomeDependencyClass
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         println(someClass.doAThing())
-        println(someClass.doSomeOtherSomething())
-        println(singletonClass.doASingletonThing())
-        println(someDependencyClass.doAThing())
     }
 }
 
 @AndroidEntryPoint
 class MyFragment : Fragment() {
-    @Inject
-    lateinit var singletonClass: SingletonClass
 }
 
 class SomeClass @Inject constructor(
-    // constructor injection
-    private val someOtherClass: SomeOtherClass
-) {
-    fun doAThing(): String = "Look I did a thing!"
-
-    fun doSomeOtherSomething(): String = someOtherClass.doSomeOtherThing()
-}
-
-class SomeOtherClass @Inject constructor() {
-    fun doSomeOtherThing(): String = "Look I did some other thing!"
-}
-
-@Singleton
-class SingletonClass @Inject constructor() {
-    fun doASingletonThing(): String = "Look I did a singleton thing!"
-}
-
-class SomeDependencyClass @Inject constructor(
-//    private val someInterface: SomeInterface,
-    // error: SomeInterface cannot be provided without an @Inject constructor or an @Provides-annotated method.
+    private val someInterfaceImpl: SomeInterface,
     private val Gson: Gson
 ) {
 
     fun doAThing(): String {
-        // error: SomeInterface cannot be provided without an @Inject constructor or an @Provides-annotated method.
-//        return "Look i got: ${someInterface.getAThing()}"
-        return "Something"
+        return "Look i got: ${someInterfaceImpl.getAThing()}"
     }
 }
 
-class SomeInterfaceImpl @Inject constructor(): SomeInterface {
-    override fun getAThing(): String = "A thing"
+class SomeInterfaceImpl @Inject constructor(
+    private val someDependency: String
+) : SomeInterface {
+    override fun getAThing(): String = "A thing, $someDependency"
 }
 
 interface SomeInterface {
     fun getAThing(): String
 }
+
+// Use Binds to tell Hilt how to create an instance of an interface
+//@InstallIn(ActivityComponent::class)
+//@Module
+//abstract class MyModule {
+//    @ActivityScoped
+//    @Binds
+//    abstract fun bindSomeDependency(someImpl: SomeInterfaceImpl): SomeInterface
+//
+//    @ActivityScoped
+//    @Binds
+//    abstract fun bindGson(gson: Gson): Gson
+//}
+
+// Use Provides to tell Hilt how to create an instance of an interface
+@InstallIn(SingletonComponent::class)
+@Module
+class MyModule {
+    @Singleton
+    @Provides
+    fun provideSomeString(): String {
+        return "Some String"
+    }
+
+    @Singleton
+    @Provides
+    fun provideSomeInterface(
+        someString: String
+    ): SomeInterface = SomeInterfaceImpl(someString)
+
+    @Singleton
+    @Provides
+    fun providesSomeGson(): Gson = Gson()
+}
+
 
